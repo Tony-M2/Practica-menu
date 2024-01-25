5b8b4d2d272aa10ab790fb5b937abadd1e18b350
Tony-M2 <155786343+Tony-M2@users.noreply.github.com>
2024-01-24 21:53:39 -0400

    Add files via upload

---
+++ Numero impar.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int aleatorio()
+{
+	return rand() % 100;
+}
+
+bool esImpar(int numero){
+	return numero %2!=0;
+}
+	
+int main(int argc, char *argv[]) {
+	int n,c=0,num;
+	cout<<"Generar n numeros Impares, Ingrese la cantida de numeros a generar: ";
+	cin>>n;
+	while(c<n)
+	{
+		num=aleatorio();
+		if(esImpar(num))
+		{
+			c++;
+			cout<<"Impar["<<c<<"]:"<<num<<endl;
+		}
+	}
+	
+	return 0;
+}
